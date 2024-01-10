79dc9d01561a27cad0b3ce0ecae5675218b38eef
Marat <milya010790@mail.ru>
2024-01-10 21:55:28 +0500

    Выполнено первое задание

---
+++ WorkWithFiles.Common/СreatingDirectoriesAndFiles.cs
@@ -0,0 +1,180 @@
+﻿using System.IO;
+
+namespace WorkWithFiles.Common
+{
+    /// <summary> </summary>
+    public class СreatingDirectoriesAndFiles
+    {
+        /// <summary>Имя основного каталога</summary>
+        public string DirectoryName { get; }
+
+        /// <summary>Начальное имя подкаталогов</summary>
+        public string SubDirectoryName { get; }
+
+        /// <summary>Начальное имя файла</summary>
+        public string FileName { get; }
+
+        /// <summary>Информация о последнем файле</summary>
+        public FileInfo LastFilesInfo { get; private set; }
+
+        /// <summary>Информация о последнем подкаталоге</summary>
+        public DirectoryInfo LastSubDirectoryInfo { get; private set; }
+
+        public СreatingDirectoriesAndFiles(string directoryName, string subDirectoryName, string fileName) 
+        {
+            if (string.IsNullOrWhiteSpace(directoryName))
+                throw new ArgumentNullException(nameof(directoryName));
+            else
+                DirectoryName= directoryName;
+
+            if (string.IsNullOrWhiteSpace(subDirectoryName))
+                throw new ArgumentNullException(nameof(subDirectoryName));
+            else
+                SubDirectoryName = subDirectoryName;
+
+            if (string.IsNullOrWhiteSpace(fileName))
+                throw new ArgumentNullException(nameof(fileName));
+            else
+                FileName = fileName;
+
+            CheckingDirectory(DirectoryName);
+        }
+
+
+        /// <summary>Создание подкаталога</summary>
+        /// <param name="minute">Интервал в минутах</param>
+        /// <returns></returns>
+        public DirectoryInfo SubDirectoryLastCreate(int minute)
+        {
+            if (LastSubDirectoryInfo == null ||
+                !LastSubDirectoryInfo.Exists ||
+                LastSubDirectoryInfo.CreationTime < DateTime.Now.Subtract(TimeSpan.FromMinutes(minute)))
+            {
+                LastSubDirectoryInfo = CreateNewSubDirectoryName(DirectoryName, SubDirectoryName);
+            }
+
+            return LastSubDirectoryInfo;
+        }
+
+        /// <summary>Создание файла в подкаталоге</summary>
+        /// <param name="second">Интервал создания файлов в секундах</param>
+        /// <returns></returns>
+        public FileInfo FilesLastCreate(int second)
+        {
+            var fileInfo = FilesLast(second); // Создаем объект класса FileInfo.
+            //Создаем файл и записываем в него.
+            using (StreamWriter sw = fileInfo.AppendText())
+            {
+                sw.WriteLine(DateTime.Now);
+            }
+
+            return FilesLast(second);
+        }
+
+
+        /// <summary>Удаление подкаталогов и вложенных файлов</summary>
+        /// <param name="folder">Путь к корневому каталогу</param>
+        public  void deleteFolder(string folder,int minute)
+        {
+            try
+            {
+                DirectoryInfo di = new DirectoryInfo(folder);
+                DirectoryInfo[] diA = di.GetDirectories();
+                FileInfo[] fi = di.GetFiles();
+                foreach (FileInfo f in fi)
+                {                
+                    
+                    if (f.CreationTime < DateTime.Now.Subtract(TimeSpan.FromMinutes(minute)))
+                    {
+                        Console.WriteLine($"Удален файл {f.Name}");
+                        f.Delete();
+                    }
+                   
+                }
+                foreach (DirectoryInfo df in diA)
+                {
+                    deleteFolder(df.FullName,minute);
+                }
+                if (di.GetDirectories().Length == 0 && di.GetFiles().Length == 0)
+                {
+                   
+                    if (di.CreationTime < DateTime.Now.Subtract(TimeSpan.FromMinutes(minute)))
+                    {
+                        Console.WriteLine($"Удален каталог {di.Name}");
+                        di.Delete();
+                    }
+                       
+                }
+                   
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Директория не найдена. Ошибка: " + ex.Message);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine("Отсутствует доступ. Ошибка: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка: " + ex.Message);
+            }
+        }
+
+        /// <summary>Поиск последнего файла в каталоге</summary>
+        /// <param name="second">Интервал создания файлов в секундах</param>
+        /// <returns></returns>
+        private FileInfo FilesLast( int second)
+        {
+            if (LastFilesInfo == null || LastFilesInfo.Exists || LastFilesInfo.CreationTime < DateTime.Now.Subtract(TimeSpan.FromSeconds(second)))
+            {
+                var files = LastSubDirectoryInfo.GetFiles().Where(file => file.
+                                CreationTime >= (DateTime.Now.Subtract(TimeSpan.FromSeconds(second)))
+                                && file.CreationTime <= DateTime.Now);
+                // Проверяем есть ли соответствие условию
+                if (!files.Any())
+                    return LastFilesInfo=CreateNewFile(LastSubDirectoryInfo.FullName, FileName);
+                else
+                {
+                    var file = files.OrderByDescending(file => file.CreationTime).First();
+                    LastFilesInfo = file;
+                    return file;
+                }
+            }
+            else
+                return LastFilesInfo;
+        }
+
+
+
+
+
+        /// <summary>Метод проверки на наличие каталога. Если нет то создаем</summary>
+        /// <param name="directory">Путь к каталогу</param>
+        private void CheckingDirectory(string directory)
+        {
+            if (!Directory.Exists(directory))
+                Directory.CreateDirectory(directory);
+        }
+
+        /// <summary>Создаем подкаталог в каталоге </summary>
+        /// <param name="directory"><inheritdoc cref="Directory" path="/summary"/></param>
+        /// <param name="SubDirectoryName">Начальное название файла</param>
+        /// <returns></returns>
+        private DirectoryInfo CreateNewSubDirectoryName(string directory, string SubDirectoryName)
+        {
+            return new DirectoryInfo(directory).CreateSubdirectory($"{SubDirectoryName}_{DateTime.Now:yyyy_MM_dd__HH_mm_ss}");
+        }
+
+        /// <summary>Создаем файл в каталоге </summary>
+        /// <param name="directory"><inheritdoc cref="Directory" path="/summary"/></param>
+        /// <param name="fileName">Начальное название файла</param>
+        /// <returns></returns>
+        private FileInfo CreateNewFile(string directory, string fileName)
+        {
+            return new FileInfo(Path.Combine(directory, $"{fileName}_{DateTime.Now:yyyy_MM_dd__HH_mm_ss}.txt"));
+        }
+
+
+    }
+}
---
+++ WorkWithFiles.Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using WorkWithFiles.Common;
+
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        СreatingDirectoriesAndFiles directoriesAndFiles = new СreatingDirectoriesAndFiles("Каталог", "Подкаталог","файл");
+        while (true)
+        {
+            // Создаем подкаталоги через заданное время
+            directoriesAndFiles.SubDirectoryLastCreate(2);
+            // В данных подкаталогах пишем файл
+            directoriesAndFiles.FilesLastCreate(20);
+
+            // Здесь метод для поиска устаревших файлов
+            directoriesAndFiles.deleteFolder("Каталог",5);
+            await Task.Delay(1000);
+        }
+        
+
+    }
+}
