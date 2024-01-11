3d1cab645bb68632f92fa3838133ef2775a5edfd
Marat <milya010790@mail.ru>
2024-01-11 09:52:15 +0500

    Добавлено решение задания 2

---
+++ WorkWithFiles.Common/DirectoryExtensions.cs
@@ -0,0 +1,48 @@
+﻿namespace WorkWithFiles.Common
+{
+    public static class DirectoryExtensions
+    {
+        public static long SizeOfFolder(string folder)
+        {
+            try
+            {
+                long catalogSize = 0;
+                //В переменную catalogSize будем записывать размеры всех файлов, с каждым
+                //новым файлом перезаписывая данную переменную
+                DirectoryInfo di = new DirectoryInfo(folder);
+                DirectoryInfo[] diA = di.GetDirectories();
+                FileInfo[] fi = di.GetFiles();
+                //В цикле пробегаемся по всем файлам директории di и складываем их размеры
+                foreach (FileInfo f in fi)
+                {
+                    //Записываем размер файла в байтах
+                    catalogSize += f.Length;
+                }
+                //В цикле пробегаемся по всем вложенным директориям директории di 
+                foreach (DirectoryInfo df in diA)
+                {
+                    //рекурсивно вызываем наш метод
+                    catalogSize+=SizeOfFolder(df.FullName);
+                }
+                //1ГБ = 1024 Байта * 1024 КБайта * 1024 МБайта
+                return catalogSize;
+              
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Директория не найдена. Ошибка: " + ex.Message);
+                return 0;
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Console.WriteLine("Отсутствует доступ. Ошибка: " + ex.Message);
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Произошла ошибка. Обратитесь к администратору. Ошибка: " + ex.Message);
+                return 0;
+            }
+        }
+    }
+}
---
+++ WorkWithFiles.Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using WorkWithFiles.Common;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите полный путь до каталога которому нужно посчитать размер: ");
+        string? put=Console.ReadLine();
+        if (!string.IsNullOrWhiteSpace(put))
+        {
+            long size = DirectoryExtensions.SizeOfFolder(put);
+            Console.WriteLine($"Размер каталога: {size} байт");
+            Console.WriteLine($"Размер каталога: {Math.Round( (double)size/1024,2)} Kb");
+            Console.WriteLine($"Размер каталога: {Math.Round((double)size / 1024 / 1024, 2)} Mb");
+            Console.WriteLine($"Размер каталога: {Math.Round((double)size / 1024 / 1024 / 1024, 2)} Gb");
+          
+        }
+        else
+        {
+            Console.WriteLine("Путь к каталогу не может быть пустым");
+        }
+        Console.ReadKey();
+    }
+}
